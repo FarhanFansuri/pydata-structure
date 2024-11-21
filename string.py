4b842840882d830dc5858f30935bbb0a8327e6f4
Farhan Fansuri <farhanfansuri2023@gmail.com>
2024-11-21 11:24:41 +0700

    init: files

---
+++ string.py
@@ -0,0 +1,12 @@
+# Indexing -> accesing data element
+# Index starts with 0
+
+x = "frog"
+print(x[3]) 
+
+# slicing
+num = [0,1,2,3,4,5,6,7,8,9]
+y = "computer"
+print(y[:-2])
+print(num[1:6:2])
+print(num[::-1])
