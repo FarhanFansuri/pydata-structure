6262b4681b69d69f5d900dfed7776b69663a16f9
Farhan Fansuri <farhanfansuri2023@gmail.com>
2024-11-21 11:41:01 +0700

    update: string.py

--- string.py
+++ string.py
@@ -10,3 +10,45 @@ y = "computer"
 print(y[:-2])
 print(num[1:6:2])
 print(num[::-1])
+print(num[1:6][::-1])
+
+# concatination
+word = "yaya " + "cika"
+print(word)
+
+list_num = [1,2,3] + [4,5]
+print(list_num)
+
+tuple_num = (1,2,3) + (4,5)
+print(tuple_num)
+
+# multiplaying
+word3 = "word " * 3
+print(word3)
+
+tuple3 = (1,2) * 3
+print(tuple3)
+
+list3 = [1,3] * 3
+print(list3)
+
+# checking membership
+b = "bug"
+print('b' in b)
+
+c = "carrot"
+print('car' in c)
+
+c = ["carrot", "banana", "apple"]
+print('carrot' not in c)
+
+# enumeration
+for i,v in enumerate(c):
+    print("index",i,":", v)
+
+# number of items
+print(len(num))
+
+# min & max
+print(min(num))
+print(max(num))
